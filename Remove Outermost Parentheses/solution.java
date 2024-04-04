4c83cc17c06f48d383da28c38d0305ab5723d077
Brijit Adak <brijitadak000@gmail.com>
2024-04-04 23:32:39 +0530

    add

--- Remove Outermost Parentheses/solution.java
+++ Remove Outermost Parentheses/solution.java
@@ -1,3 +1,27 @@
+/*
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+*/
+
 class Solution {
     public String removeOuterParentheses(String s) {
         Stack<Character> st = new Stack<>();
