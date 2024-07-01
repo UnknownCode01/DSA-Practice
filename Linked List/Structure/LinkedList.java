b3a979ac8467bf5c9e9bc25fd84005c810da687d
Brijit Adak <brijitadak000@gmail.com>
2024-07-01 19:29:31 +0530

    add

--- Linked List/Structure/LinkedList.java
+++ Linked List/Structure/LinkedList.java
@@ -8,6 +8,7 @@ class LinkedList{
         } 
     }
     public void addFirst(int data){
+        nodes++;
         Node newNode=new Node(data);
         if(head==null){
             head=tail=newNode;
@@ -16,9 +17,40 @@ class LinkedList{
 
         newNode.next=head;
         head=newNode;
+    }
+    public void addLast(int data){
+        nodes++;
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void add(int idx, int data){
+        if(idx>nodes){
+            System.out.println("Index exceeds nodes");
+            return;
+        }
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        nodes++;
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        Node newNode=new Node(data);
+        newNode.next=temp.next;
+        temp.next=newNode;
 
     }
-    public void print(Node head){
+    public void print(){
         if(head==null){
             System.out.println("No data exists.");
             return;
@@ -32,11 +64,16 @@ class LinkedList{
     }
     public static Node head;
     public static Node tail;
+    public static int nodes;
     public static void main(String[] args){
         LinkedList l1 = new LinkedList();
         l1.addFirst(5);
         l1.addFirst(4);
         l1.addFirst(7);
-        l1.print(head);
+        l1.addLast(1);
+        l1.addLast(9);
+        l1.add(6,99);
+        l1.add(5,0);
+        l1.print();
     }
 }
