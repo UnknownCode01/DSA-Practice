1fb102e5a21580ee4438343325a44af12bc0dce5
Brijit Adak <brijitadak000@gmail.com>
2024-08-12 19:27:09 +0530

    add

---
+++ Stack/Next Greater Element/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution{
+    public static void main(String[] args){
+        int[] arr = {6,8,0,1,3};
+        Stack<Integer> s = new Stack<>();
+        int[] nextGreater = new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && arr[i]>=arr[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextGreater[i]=-1;
+            }else{
+                nextGreater[i]=arr[s.peek()];
+            }
+            s.push(i);
+        }
+        for(int i=0;i<nextGreater.length;i++){
+        System.out.print(nextGreater[i]+" ");
+        }
+    }
+}
