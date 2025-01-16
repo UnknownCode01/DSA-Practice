a730cd2379812185c03404d0895717b9cfb09ac0
Brijit Adak <brijitadak000@gmail.com>
2025-01-17 02:45:14 +0530

    add

---
+++ Linked List/Merge Two Sorted Lists/Approach 2/Solution.java
@@ -0,0 +1,67 @@
+class Solution {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // Merge two sorted linked lists
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode cur = prehead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
+    // Utility function to print the linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create first sorted list: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // Create second sorted list: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        // Create Solution object to use the merge method
+        Solution solution = new Solution();
+        ListNode mergedList = solution.mergeTwoLists(l1, l2);
+
+        // Print the merged list
+        printList(mergedList);
+    }
+}
