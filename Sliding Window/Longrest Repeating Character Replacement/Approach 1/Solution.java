620e1f75db92b0a3adedb9d8a5750cc7f782ea7a
Brijit Adak <brijitadak000@gmail.com>
2025-01-31 20:50:14 +0530

    add

---
+++ Sliding Window/Longrest Repeating Character Replacement/Approach 1/Solution.java
@@ -0,0 +1,35 @@
+//O(2N)
+import java.util.*;
+class Solution{
+    public static int characterReplacement(String s, int k) {
+        int l=0,r=0;
+        int n = s.length();
+        int maxf = 0;
+        int maxLength = 0;
+        int[] hash = new int[26];
+        while(r<n){
+            hash[s.charAt(r)-'A']++;
+            maxf = Math.max(maxf,hash[s.charAt(r)-'A']);
+            int changes = r-l+1-maxf;
+            while(changes>k){
+                hash[s.charAt(l)-'A']--;
+                maxf = 0;
+                for(int i=0;i<26;i++){
+                    maxf = Math.max(maxf,hash[i]);
+                }
+                l++;
+                changes = r-l+1-maxf;
+            }
+            if(r-l+1-maxf<=k){
+                maxLength = Math.max(maxLength,r-l+1);
+            }
+            r++;
+        }
+        return maxLength;
+    }
+    public static void main(String[] args){
+        String s = "AABABBA";
+        int k = 2;
+        System.out.println(characterReplacement(s,k));
+    }
+}
