699e64565dba35e6dbca5f9873195c28445c2a6b
Brijit Adak <brijitadak000@gmail.com>
2025-03-09 18:16:31 +0530

    add

--- Array/3 Sum Closest/Solution.java
+++ Array/3 Sum Closest/Solution.java
@@ -1,5 +1,40 @@
+import java.util.*;
+
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int mini = Integer.MAX_VALUE;
+        int ans = target;
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                // System.out.println("sum : " + sum);
+                int dis = Math.abs(sum - target);
+                if(dis<mini){
+                    mini = dis;
+                    ans = sum;
+                }
+                // System.out.println("mini: " + mini);
+                if (sum > target) {
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    return sum;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 2, 1, 4 };
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
     }
 }
