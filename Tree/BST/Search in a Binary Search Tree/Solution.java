58a1a089d5d2ed4866a0f039ec8d2243c0d71276
Brijit Adak <brijitadak000@gmail.com>
2025-03-11 02:34:39 +0530

    add

---
+++ Tree/BST/Search in a Binary Search Tree/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null)
+            return null;
+        if (val == root.val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        TreeNode ans = searchBST(root,2);
+        int check = 2;
+        System.out.println(ans.val==check);
+    }
+}
