f15eba80b0a3b373cb1b652015c5fc68803fc1d0
Brijit Adak <brijitadak000@gmail.com>
2025-04-20 22:28:13 +0530

    add

---
+++ DP/T3 - Largest Divisible Subset/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution{
+    public static List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int[] dp = new int[nums.length];
+        int[] prev = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+        int maxi = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxi]) maxi = i;
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = maxi; i >= 0; i = prev[i]) {
+            res.add(nums[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        int[] nums = {1,2,3,8,9,18,27,36};
+        System.out.println(largestDivisibleSubset(nums));
+    }
+}
