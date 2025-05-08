42a1a09054da90a1aa6ae65e16a5f3868073aa6b
Brijit Adak <brijitadak000@gmail.com>
2025-05-08 23:43:45 +0530

    add

--- Miscellaneous/Number Line Jumps/NumberLineJumps.java
+++ Miscellaneous/Number Line Jumps/NumberLineJumps.java
@@ -1,10 +1,4 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
 
 class Result {
     public static String kangaroo(int x1, int v1, int x2, int v2) {
