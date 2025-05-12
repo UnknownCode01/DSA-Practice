8060f52d856149e8020199f37751047f26aeb108
Brijit Adak <brijitadak000@gmail.com>
2025-05-12 20:43:29 +0530

    add

---
+++ Sliding Window/Count Subarrays Where Max Element Appears at Least K Times/Approach 1/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static long countSubarrays(int[] nums, int k) {
+        int maxi = Integer.MIN_VALUE;
+        for (int num : nums)
+            maxi = Math.max(maxi, num);
+        int left = 0;
+        long maxiOccurence = 0;
+        long res = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == maxi)
+                maxiOccurence++;
+            while (maxiOccurence >= k) {
+                if (nums[left] == maxi)
+                    maxiOccurence--;
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 2, 3, 3 };
+        int k = 2;
+        System.out.println(countSubarrays(nums, k));
+    }
+}
