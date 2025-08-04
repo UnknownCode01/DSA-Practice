b6cf70cb4fd50f5dab72a573e41702ccd619fe95
Brijit Adak <brijitadak000@gmail.com>
2025-08-04 23:14:51 +0530

    add

---
+++ Pattern/Hollow Diamond Inside Filled Square/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static void f(int n) {
+        // Upper
+        int spaces = 0;
+        for (int i = 0; i < n; i++) {
+            // stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            spaces += 2;
+        }
+        // Lower
+        spaces = 2*(n-1);
+        for (int i = 0; i < n; i++) {
+            // stars
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            spaces -= 2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        f(n);
+    }
+}
